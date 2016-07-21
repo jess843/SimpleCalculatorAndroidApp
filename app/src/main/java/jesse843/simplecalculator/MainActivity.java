15df44045a570a2a0e9244a4f517cc0725f3bf87
Jesse DeJong <jessedejong101@gmail.com>
2016-07-21 13:49:17 -0500

    Adding functioning

--- app/src/main/java/jesse843/simplecalculator/MainActivity.java
+++ app/src/main/java/jesse843/simplecalculator/MainActivity.java
@@ -8,12 +8,23 @@ import android.support.v7.widget.Toolbar;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.ImageSpan;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
 
 public class MainActivity extends AppCompatActivity {
+    String previous;
+    private TextView displayTextView;
+    private boolean multiply;
+    private boolean divide;
+    private boolean add;
+    private boolean subtract;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +32,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //initializing variables
+        previous = "0";
+        displayTextView  = (TextView)findViewById(R.id.displayTextView);
+        multiply = false;
+        divide = false;
+        add = false;
+        subtract = false;
     }
 
     @Override
@@ -43,5 +62,117 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void digitOne (View v) {
+        digitAdd("1");
+    }
+
+    public void digitTwo (View v) {
+        digitAdd("2");
+    }
+
+    public void digitThree (View v) {
+        digitAdd("3");
+    }
+
+    public void digitFour (View v) {
+        digitAdd("4");
+    }
+
+    public void digitFive (View v) {
+        digitAdd("5");
+    }
+
+    public void digitSix (View v) {
+        digitAdd("6");
+    }
+
+    public void digitSeven (View v) {
+        digitAdd("7");
+    }
+
+    public void digitEight (View v) {
+        digitAdd("8");
+    }
+
+    public void digitNine (View v) {
+        digitAdd("9");
+    }
+
+    public void digitZero (View v) {
+        digitAdd("0");
+    }
+
+    public void digitAdd(String digit) {
+        if (displayTextView.getText().toString().contains(".")) {
+            displayTextView.setText(""+displayTextView.getText().toString()+digit);
+        }
+        else {
+            BigInteger displayNum = new BigInteger(displayTextView.getText().toString());
+            displayNum = displayNum.multiply(new BigInteger("10"));
+            displayNum = displayNum.add(new BigInteger(digit));
+            displayTextView.setText(""+displayNum);
+        }
+    }
+
+    public void addDecimal (View v) {
+        if (!displayTextView.getText().toString().contains(".")) {
+            displayTextView.setText(displayTextView.getText().toString() + ".");
+        }
+    }
+
+    public void clearDisplay (View v) {
+        displayTextView.setText("0");
+    }
+
+    public void backspace (View v) {
+        if (displayTextView.getText().toString().length() > 1)
+            displayTextView.setText(displayTextView.getText().toString().substring(0, displayTextView.getText().toString().length()-1));
+        else
+            displayTextView.setText("0");
+    }
+
+    public void negate (View v) {
+        if (!displayTextView.getText().toString().contains(".")) {
+            BigInteger displayNum = new BigInteger(displayTextView.getText().toString());
+            displayNum = displayNum.negate();
+            displayTextView.setText(""+displayNum);
+        }
+        else {
+            BigDecimal displayNum = new BigDecimal(displayTextView.getText().toString());
+            displayNum = displayNum.negate();
+            displayTextView.setText(""+displayNum);
+        }
+    }
+
+    public void multiply (View v) {
+        if (!divide && !add && !subtract && displayTextView.getText().toString().equals("0")) {
+            previous = displayTextView.getText().toString();
+            multiply = true;
+        }
+    }
+
+    public void equals (View v) {
+        if (!displayTextView.getText().toString().equals("0") && (multiply || divide || add || subtract)) {
+            BigDecimal result = new BigDecimal("0");
+            if (multiply) {
+                result = new BigDecimal(previous);
+                result = result.multiply(new BigDecimal(displayTextView.getText().toString()));
+            }
+            else if (divide) {
+                result = new BigDecimal(previous);
+                result = result.divide(new BigDecimal(displayTextView.getText().toString()));
+            }
+            else if (add) {
+                result = new BigDecimal(previous);
+                result = result.add(new BigDecimal(displayTextView.getText().toString()));
+            }
+            else if (subtract) {
+                result = new BigDecimal(previous);
+                result = result.subtract(new BigDecimal(displayTextView.getText().toString()));
+            }
+            displayTextView.setText(""+result);
+        }
     }
 }
