07fecb8c52ca37bf5505b18e60a2280db513a1b8
Jesse DeJong <jessedejong101@gmail.com>
2016-07-20 21:28:42 -0500

    Completing layout

--- app/src/main/java/jesse843/simplecalculator/MainActivity.java
+++ app/src/main/java/jesse843/simplecalculator/MainActivity.java
@@ -5,9 +5,13 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ImageSpan;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
